504f45f408bf5ae65e9bf10d7fc7c295920b7d9b
acrofts040 <54963638+acrofts040@users.noreply.github.com>
2021-06-22 22:24:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function init()
 				var landmark4 = new google.maps.LatLng(42.3472, -71.0802);
 				var landmark5 = new google.maps.LatLng(42.3663, -71.0544);
 				var landmark6 = new google.maps.LatLng(	42.3542, -71.0704);
-				var curlocation = new google.maps.LatLng(42.3542, -71.0704);
+				var curlocation = new google.maps.LatLng(42.01, -71.0704);
 				
 				navigator.geolocation.getCurrentPosition((position) => {
 					console.log("OUT");
