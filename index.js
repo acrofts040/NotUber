c61f10b1fdd51e3e99c0d06f849ffc58f43aa9bf
acrofts040 <54963638+acrofts040@users.noreply.github.com>
2021-07-04 03:25:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function init()
 				// Set up map with marker at 
 				var myOptions = {
 					zoom: 3, // The larger the zoom number, the bigger the zoom
-					center: curlatlng,
+					center: landmark0,
 					mapTypeId: google.maps.MapTypeId.ROADMAP
 				};
 				
