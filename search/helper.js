37071c7a44ac5513d1dccd1f7649d539844c4359
Andrew Crofts <54963638+acrofts040@users.noreply.github.com>
2022-01-13 17:06:24 -0500

    create helper.js
    wanted to add smooth zoom in beginning

---
+++ search/helper.js
@@ -0,0 +1,15 @@
+
+// smooth zoom function
+function smoothZoom (map, max, cnt) {
+    if (cnt >= max) {
+        return;
+    }
+    else {
+        z = google.maps.event.addListener(map, 'zoom_changed', function(event){
+            google.maps.event.removeListener(z);
+            smoothZoom(map, max, cnt + 1);
+        });
+        setTimeout(function(){map.setZoom(cnt)}, 80); // 80ms is what I found to work well on my system -- it might not work well on all systems
+    }
+} 
+
