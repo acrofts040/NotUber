7b0471dba7283a6b3a550ddfc8628dd8850c30fa
Andrew Crofts <54963638+acrofts040@users.noreply.github.com>
2022-01-13 17:13:02 -0500

    Update helpers.js

--- search/helpers.js
+++ search/helpers.js
@@ -13,3 +13,11 @@ function smoothZoom (map, max, cnt) {
     }
 } 
 
+function animateMapZoomTo(map, targetZoom) {
+    var currentZoom = arguments[2] || map.getZoom();
+    if (currentZoom != targetZoom) {
+        google.maps.event.addListenerOnce(map, 'zoom_changed', function (event) {
+            animateMapZoomTo(map, targetZoom, currentZoom + (targetZoom > currentZoom ? 1 : -1));
+        });
+        setTimeout(function(){ map.setZoom(currentZoom) }, 80);
+    }
